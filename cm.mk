97eb04fa782fa99dad9e82acb3afd81cc3aff142
chain01 <qq164550073@163.com>
2018-04-12 19:23:14 +0800

    第一次提交，使用cm自带rec

---
+++ cm.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Release name
+PRODUCT_RELEASE_NAME := nx541j
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit device configuration
+$(call inherit-product, device/zte/nx541j/device_nx541j.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Device identifier
+PRODUCT_DEVICE := nx541j
+PRODUCT_NAME := cm_nx541j
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := nx541j
+PRODUCT_MANUFACTURER := zte
+
+
+# Time Zone data for Recovery
+PRODUCT_COPY_FILES += \
+	bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
